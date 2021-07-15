e29dd207f43443afd7584deb8ae8bdf80ed8e0f7
mrhofm <87458714+mrhofm@users.noreply.github.com>
2021-07-15 12:56:48 +0800

    Initial commit

---
+++ data.js
@@ -0,0 +1,62 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-3d-room-17901",
+      "name": "3D room 17901",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.9731610561379664,
+        "pitch": 0.15633125097789247,
+        "fov": 1.1989879464110744
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3504817253566728,
+          "pitch": 0.2716571489787647,
+          "rotation": 0,
+          "target": "0-3d-room-17901"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.227928865606998,
+          "pitch": 0.07832811076373503,
+          "title": "Noah's bed",
+          "text": "This is the bed that Noah is supposed to sleep in."
+        },
+        {
+          "yaw": 2.1195491911555013,
+          "pitch": 0.4897078044836132,
+          "title": "Noah's desk",
+          "text": "Here, homework should be done."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
