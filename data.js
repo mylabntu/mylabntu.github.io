ee48b46109876d6b1ca065fcc804ee88b52fe48f
mylabntu <87461513+mylabntu@users.noreply.github.com>
2021-07-23 13:12:01 +0800

    Tried html

--- data.js
+++ data.js
@@ -41,43 +41,7 @@ var APP_DATA = {
           "yaw": -0.227928865606998,
           "pitch": 0.07832811076373503,
           "title": "Noah's bed",
-           "text": "<HTML>
-
-<HEAD>
-
-<TITLE>Your Title Here</TITLE>
-
-</HEAD>
-
-<BODY BGCOLOR="FFFFFF">
-
-<CENTER><IMG SRC="clouds.jpg" ALIGN="BOTTOM"> </CENTER>
-
-<HR>
-
-<a href="http://somegreatsite.com">Link Name</a>
-
-is a link to another nifty site
-
-<H1>This is a Header</H1>
-
-<H2>This is a Medium Header</H2>
-
-Send me mail at <a href="mailto:support@yourcompany.com">
-
-support@yourcompany.com</a>.
-
-<P> This is a new paragraph!
-
-<P> <B>This is a new paragraph!</B>
-
-<BR> <B><I>This is a new sentence without a paragraph break, in bold italics.</I></B>
-
-<HR>
-
-</BODY>
-
-</HTML>"
+           "text": "<a href='http://www.google.com'>This is a link</a> this is not a link."
         },
         {
           "yaw": 2.1195491911555013,
